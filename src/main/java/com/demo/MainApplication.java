3954949eae992ac0d80f82c52a6a3ae6af6c78d0
Ben <1192066534@qq.com>
2019-03-12 16:37:53 +0800

    SpringBoot的模板

--- src/main/java/com/demo/MainApplication.java
+++ src/main/java/com/demo/MainApplication.java
@@ -1,15 +1,10 @@
 package com.demo;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.demo.annotation.Test;
 
 //配置SpringBoot全局开关
 @SpringBootApplication
@@ -22,14 +17,4 @@ public class MainApplication
         SpringApplication.run(MainApplication.class, args);
     }
     
-    @Test("测试")
-    @RequestMapping("/")
-    public List<Object> getID()
-    {
-        List<Object> list = new ArrayList<>();
-        list.add("Huang");
-        list.add(null);
-        list.add(1);
-        return list;
-    }
 }
---
+++ src/test/java/com/huang/test/ApplicationTest.java
@@ -0,0 +1,21 @@
+package com.huang.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTest
+{
+    
+    @Test
+    public void contentTest()
+    {
+        fail("Not yet implemented");
+    }
+    
+}
